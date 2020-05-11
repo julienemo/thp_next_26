2d23436ec77c448be3b1deafc173b333dc83c543
julienemo <juliette.nada@gmail.com>
2020-05-11 12:08:09 +0200

    [project] add Home link - installed node-sass & antD

--- professionencyclopedia/src/App.js
+++ professionencyclopedia/src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+import Home from "./Pages/Home";
+import Navbar from "./Components/Navbar"
+
+const App = () => (
+  <Router>
+    <div>
+      <Navbar />
+      <Switch>
+        <Route path="/">
+          <Home/>
+        </Route>
+      </Switch>
     </div>
-  );
-}
+</Router>
+);
+
 
 export default App;
